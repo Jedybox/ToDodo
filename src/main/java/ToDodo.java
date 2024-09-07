a5a16d3065d864c4236d912faeeedc5bc7c664eb
Jedy <johnericssonytac@gmail.com>
2024-09-07 16:27:27 +0800

    WTF AM I DOING

--- src/main/java/ToDodo.java
+++ src/main/java/ToDodo.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ToDodo extends JFrame {
-
-
+    private Point initialClick;
 
     ToDodo () {
 
@@ -14,9 +15,11 @@ public class ToDodo extends JFrame {
         titleBar.setPreferredSize(new Dimension(100, 50));
         titleBar.setLayout(new BorderLayout());
 
-        JLabel title = new JLabel("ToDodo", SwingConstants.CENTER);
-        title.setForeground(Color.WHITE);
-        titleBar.add(title, BorderLayout.WEST);
+        JPanel iconAndTitle = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
+        iconAndTitle.setOpaque(false);
+
+        JLabel icon = new JLabel(new ImageIcon("resources/tododo.png"));
+        JLabel title = new JLabel("ToDodo");
 
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
         buttonPanel.setOpaque(false);
@@ -26,22 +29,59 @@ public class ToDodo extends JFrame {
         closeButton.setBackground(Color.RED);
         closeButton.setFocusPainted(false);
         closeButton.setBorderPainted(false);
-        closeButton.setPreferredSize(new Dimension(40, 40));
+        closeButton.setPreferredSize(new Dimension(50, 50));
 
         JButton minimizeButton = new JButton("-");
         minimizeButton.setForeground(Color.WHITE);
         minimizeButton.setBackground(Color.GRAY);
         minimizeButton.setFocusPainted(false);
         minimizeButton.setBorderPainted(false);
-        minimizeButton.setPreferredSize(new Dimension(40, 40));
+        minimizeButton.setPreferredSize(new Dimension(50, 50));
 
         closeButton.addActionListener(e -> System.exit(0));
         minimizeButton.addActionListener(e -> setState(JFrame.ICONIFIED));
 
+        titleBar.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                initialClick = e.getPoint();
+                getComponentAt(initialClick);
+            }
+        });
+
+        titleBar.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                // Get the current location of the window
+                int thisX = getLocation().x;
+                int thisY = getLocation().y;
+
+                // Determine how much the mouse moved since the initial click
+                int xMoved = e.getX() - initialClick.x;
+                int yMoved = e.getY() - initialClick.y;
+
+                // Move the window
+                int x = thisX + xMoved;
+                int y = thisY + yMoved;
+                setLocation(x, y);
+            }
+        });
+
+        title.setVisible(true);
+        icon.setVisible(true);
+        iconAndTitle.add(title);
+        iconAndTitle.add(icon);
+        closeButton.setVisible(true);
+        minimizeButton.setVisible(true);
+        buttonPanel.setVisible(true);
+
         buttonPanel.add(minimizeButton);
         buttonPanel.add(closeButton);
+        titleBar.add(buttonPanel);
+        titleBar.add(iconAndTitle, BorderLayout.WEST);
 
-        this.setSize(500, 500);
+        titleBar.setVisible(true);
+        this.add(titleBar, BorderLayout.NORTH);
+        this.setSize(1000, 700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
