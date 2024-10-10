26d304ee578affa9dc415513348042c1e8765245
Jedy <johnericssonytac@gmail.com>
2024-10-10 18:35:15 +0800

    Add JTextField and JTextArea to AddTodoPage for user input

--- src/main/java/AddTodoPage.java
+++ src/main/java/AddTodoPage.java
@@ -1,13 +1,15 @@
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class AddTodoPage extends JPanel implements ActionListener {
-    
+    JTextField titleField;
+    JTextArea descriptionField;
+
     AddTodoPage() {
-        
+
     }
 
     @Override   
